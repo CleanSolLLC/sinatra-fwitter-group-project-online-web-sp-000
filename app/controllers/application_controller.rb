37b2ef3993185560a10e62b23f9897b010e5c73c
Mark May <mamark90@gmail.com>
2021-03-03 02:49:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,24 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'mission_impossible'
+  end
+
+  get '/' do
+  	erb :'index'
+  end
+
+  helpers do
+  	
+  	def is_logged_in?(session)
+  	  !!current_user(session)
+  	end
+
+  	def current_user(session)
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]	
+  	end
+
   end
 
 end
